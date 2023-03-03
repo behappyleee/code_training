e4d50293987aa635ac105e9a6de3d86f51de8baf
leechulhan <dhfhfkwhdk@naver.com>
2023-03-03 20:58:54 +0900

    자바의 정석 COMMIT

---
+++ java_practice/src/inheritance/ModifierTest.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class ModifierTest {
+	
+	// 제어자 (Modifier)
+	
+	// 제어자는 클래스나 멤버변수나 메서드에 주로 사용 됨
+	
+	// 접근 제어자 - public / protected / default / private
+	// 그 외 - static / final / abstract / native / transient / synchronized / volatile / strictfp
+	
+	
+	
+}
