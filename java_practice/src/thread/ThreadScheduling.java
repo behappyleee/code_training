94d5b80336188cf533c51a0c25e45ee99bb248c9
leechulhan <dhfhfkwhdk@naver.com>
2023-03-09 23:12:03 +0900

    Java 의 정석  학습 COMMIT

--- java_practice/src/thread/ThreadScheduling.java
+++ java_practice/src/thread/ThreadScheduling.java
@@ -9,5 +9,9 @@ public class ThreadScheduling {
 	// sleep -> 일정 시간 동안 쓰레드를 멈추게 한다.
 	
 	// TODO 750 page Thread sleep  학습 하기 !!!!
+	// sleep() 에 의해 일시정지 상태가 된 쓰레드는 지저된 시간이 다 되거나 interrupt() 가 호출 되면 (InterruptException)
+	// 발생하여 잠에서 깨어나 실행 대기 상태가 된다.
+	
+	
 	
 }
