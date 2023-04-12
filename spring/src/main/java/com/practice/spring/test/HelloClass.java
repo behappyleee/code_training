7dca196f098f8e439f2c94c1682a3f7b31b4322b
leechulhan <dhfhfkwhdk@naver.com>
2023-04-12 23:55:43 +0900

    Javascript commit

---
+++ spring/src/main/java/com/practice/spring/test/HelloClass.java
@@ -0,0 +1,21 @@
+package com.practice.spring.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloClass {
+
+    Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping("/test")
+    public String helloWorld() {
+        logger.debug("Hello World This Project !");
+        return "HelloWolrd";
+    }
+
+
+}
