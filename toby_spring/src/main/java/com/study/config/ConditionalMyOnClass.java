2c4c74514c2b7a6f4cc7560d104baa486878a444
leechulhan <dhfhfkwhdk@naver.com>
2023-04-28 23:57:57 +0900

    Springboot Commit

--- toby_spring/src/main/java/com/study/config/ConditionalMyOnClass.java
+++ toby_spring/src/main/java/com/study/config/ConditionalMyOnClass.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.METHOD})
-@Conditional(MyOnClassCondition.class)
+@Conditional(MyOnClassCondition.class)  // 해당 클래스에서 어노테이션을 설정 해줌 (True or False)
 public @interface ConditionalMyOnClass {
     String value();
 }
