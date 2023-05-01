77413ea9000550b4ad2939291a67bcd1c01bc2f9
leechulhan <dhfhfkwhdk@naver.com>
2023-05-01 23:54:42 +0900

    JavaScript Studying Commit

--- javascript-exercises-main/03_reverseString/reverseString.js
+++ javascript-exercises-main/03_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reverseStr = '';
+    if(!str) {
+        return reverseStr;
+    }
+    let strArr = str.split('');
 
+    for(let i = (strArr.length-1); i >= 0; i--){
+        reverseStr += strArr[i];    
+    }
+
+    console.log('reverseStr Test : ' + reverseStr);
+
+    return reverseStr;
 };
 
 // Do not edit below this line
--- javascript-exercises-main/04_removeFromArray/removeFromArray.js
+++ javascript-exercises-main/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...filterArgs) {
+    let filteredArr = [];
+    let filteredArgs = [...filterArgs];
 
+    for(let i = 0; i<arr.length; i++) {
+        if(!filteredArgs.includes(arr[i])) {
+            filteredArr.push(arr[i]);
+        }
+    }
+
+    console.log('FILTERED ARR DATA TEST : ' + JSON.stringify(filteredArr));
+
+    return filteredArr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
