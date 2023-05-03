2c8d9c3674d4683b2b827d3404a23b57e20d96be
leechulhan <dhfhfkwhdk@naver.com>
2023-05-03 23:52:25 +0900

    JavaScript Studying Commit

--- javascript-exercises-main/08_calculator/calculator.js
+++ javascript-exercises-main/08_calculator/calculator.js
@@ -1,26 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a+b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return (a-b);
 };
 
-const sum = function() {
-	
+const sum = function(...args) {
+	let arrSum = 0;
+  let sumTest = args.forEach((item) => {
+    return (arrSum += item)
+  });
+  
+  console.log(sumTest);
+
+  return arrSum;  
 };
 
-const multiply = function() {
-
+const multiply = function(a ,b) {
+  return (a*b);
 };
 
-const power = function() {
-	
+const power = function(num) {
+  let loopNum = num;
+  let returnNum = 1;
+  while(loopNum > 0) {
+    returnNum *= num;
+    loopNum--;
+  }
+  return returnNum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  return recursiveFactorial(num);
 };
+
+const recursiveFactorial = function (num) {
+  if(num == 0) return;
+  return (num * recursiveFactorial(--num));
+}
+
 
 // Do not edit below this line
 module.exports = {
