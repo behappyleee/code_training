c1d766b73e08d6368e9d920a93297af5f49f3506
leechulhan <dhfhfkwhdk@naver.com>
2023-05-07 01:32:37 +0900

    toby_spring 초기화

---
+++ toby_spring/study_toby/src/main/java/com/my/study_toby/StudyTobyApplication.java
@@ -0,0 +1,13 @@
+package com.my.study_toby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTobyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyTobyApplication.class, args);
+	}
+
+}
---
+++ toby_spring/study_toby/src/test/java/com/my/study_toby/StudyTobyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.study_toby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTobyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
