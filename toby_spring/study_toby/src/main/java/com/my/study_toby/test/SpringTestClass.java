417029c18072ede6ab0ce7a1d7a6765f8f992d8a
behappyleee <dhfhfkwhdk@naver.com>
2023-05-22 22:58:28 +0900

    branch_tst commit test

--- toby_spring/study_toby/src/main/java/com/my/study_toby/test/SpringTestClass.java
+++ toby_spring/study_toby/src/main/java/com/my/study_toby/test/SpringTestClass.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class SpringTestClass {
 
+     // Spring Test Class Commit
+     // Branch Commit Test
      {
         System.out.println("THIS IS SPRING TEST CLASS CLASS THEST CHECK");
      }
