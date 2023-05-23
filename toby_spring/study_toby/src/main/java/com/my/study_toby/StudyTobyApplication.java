544a0ee07adf2d4f328450bedac414d6e07dfbc8
behappyleee <dhfhfkwhdk@naver.com>
2023-05-23 23:38:47 +0900

    Java Script Studying commit

--- toby_spring/study_toby/src/main/java/com/my/study_toby/StudyTobyApplication.java
+++ toby_spring/study_toby/src/main/java/com/my/study_toby/StudyTobyApplication.java
@@ -4,9 +4,14 @@ import com.my.study_toby.config.TobyApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.ServerSocket;
+
 // @SpringBootApplication
 @TobyApplication
 public class StudyTobyApplication {
+
+	ServerSocket sc;
+
 
 	// This is Test
 	// FROM my_toby Branch Test
